32c172cb0b16222a20eb7b97f16be3bf6e7e11b3
饶启玉 <raoqiyu@yeah.net>
2019-09-12 11:20:38 +0800

    return list

--- python/cumlLib/sort/sort.pyx
+++ python/cumlLib/sort/sort.pyx
@@ -7,12 +7,12 @@ def sort(nums):
     cdef size_t n_samples = len(nums)
 
     cdef array.array h_in = array.array('I',nums)
-    cdef array.array h_out = array.array('I')
+    cdef array.array h_out = array.array('I',[0 for _ in range(n_samples)])
     cdef unsigned int[:] h_out_py = h_out
 
 
     radix_sort(h_in.data.as_uints, h_out.data.as_uints, n_samples)
 
     del h_in
-    return h_out_py
+    return h_out.tolist()
     # return nums_sorted
