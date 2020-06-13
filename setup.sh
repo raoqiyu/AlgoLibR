036884fcc4e43d41bebab509a37008c1cadf5c4f
raoqiyu <raoqiyu@yeah.net>
2020-06-13 23:18:20 +0800

    不删除build,下次复用，不重复编译未改动的文件

--- setup.sh
+++ setup.sh
@@ -14,10 +14,8 @@ cd build
 cmake ..
 make -j
 make install
-cd ../../
-rm -rf build
 
-cd python
+cd ../../python
 # python setup.py build_ext
 python setup.py install
 rm -rf AlgoLibR.egg-info build dist
