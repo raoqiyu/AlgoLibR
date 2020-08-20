c4163767e7acc803c15a90952b7966fa7cb0fd61
raoqiyu <raoqiyu@yeah.net>
2020-08-20 20:49:34 +0800

    fix bug : illegal line position

--- cpp/AlgoLibR/ai/nlp/mining/word/NewWordExtractor.cpp
+++ cpp/AlgoLibR/ai/nlp/mining/word/NewWordExtractor.cpp
@@ -29,6 +29,7 @@ inline void NewWordExtractor::AddBeginWord(std::wstring &line) {
     }
     if (is_ok) {
         for (auto k = this->min_n - 1; k < this->max_n; k++) {
+            if(start_pos + k >= line.size()) break;
             if (delimiters.find(line[start_pos + k]) != delimiters.end()) break;
             last_wchar_ptr = AddNGram(line.substr(start_pos, k + 1).c_str());
             auto iter = this->m_words.find(last_wchar_ptr);
@@ -75,14 +76,15 @@ void NewWordExtractor::Extract(const char *src_fname) {
 
     std::wstring line;
     std::wstring gram_str;
-    unsigned long start_pos, k = this->max_n;
+    unsigned long start_pos, split_pos, k = this->max_n;
     while (std::getline(src_file, line)) {
         if (line.size() < this->min_n) continue;
         AddBeginWord(line);
-        for (start_pos = 1; start_pos < line.size() - this->max_n; start_pos++) {
+        split_pos = std::max((long)(line.size()-this->max_n), 0L);
+        for (start_pos = 1; start_pos < split_pos; start_pos++) {
             AddWord(line, start_pos, this->max_n);
         }
-        for (start_pos = line.size() - k; start_pos < line.size() && k > 0; start_pos++, k--) {
+        for (start_pos = split_pos; start_pos < line.size() && k > 0; start_pos++, k--) {
             AddWord(line, start_pos, k);
         }
     }
