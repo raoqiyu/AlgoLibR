a15661fa93d18b3effff691a6a72caf08fac8439
raoqiyu <raoqiyu@yeah.net>
2020-10-18 14:08:23 +0800

    change type

--- cpp/AlgoLibR/data_structure/trie/ac_double_array_trie.hpp
+++ cpp/AlgoLibR/data_structure/trie/ac_double_array_trie.hpp
@@ -1,5 +1,6 @@
 
 #include <vector>
+#include <queue>
 #include <unordered_set>
 #include <unordered_map>
 
@@ -7,7 +8,7 @@ namespace AlgoLibR::data_structure::trie::ac_dat{
 
 class ACDATNode{
  public:
-  explicit ACDATNode(wchar_t chr): chr(chr), key_index(0){}
+  explicit ACDATNode(wchar_t chr): chr(chr), key_index(0), base_index(0){}
   ~ACDATNode(){
       for(auto child_node:this->child_nodes){
           delete child_node;
@@ -41,8 +42,21 @@ class ACDATNode{
       return this->child_nodes.size();
   }
 
+  bool isExists(wchar_t key){
+      auto iter = this->child_indices.find(key);
+      return iter != this->child_indices.end();
+  }
+
+  ACDATNode* getChild(wchar_t key){
+      auto iter = this->child_indices.find(key);
+      if(iter == this->child_indices.end()) return nullptr;
+
+      return this->child_nodes[iter->second];
+  }
+
   wchar_t chr;
-  size_t  key_index;
+  ACDATNode *failure;
+  size_t  key_index, base_index;
   std::unordered_map<wchar_t, size_t> child_indices;
   std::vector<ACDATNode*> child_nodes;
 };
@@ -62,14 +76,16 @@ class AhoCorasickDoubleArrayTrie{
       buildDoubleArrayTrie();
 
       // construct failure array & output array
-//      constructFailure();
+      constructFailure();
 
+      // delete trie Tree
+//      delete this->m_root;
   }
 
   V get(std::wstring &key){
       auto idx = exactMatchSearch(key);
       if(idx < 0){
-          return NULL;
+          return (V) NULL;
       }
       return this->m_values[idx-1];
   }
@@ -97,18 +113,107 @@ class AhoCorasickDoubleArrayTrie{
       return result;
   }
 
+  std::vector<std::pair<size_t, std::wstring>> ParseText(const std::wstring &text){
+      std::vector<std::pair<size_t, std::wstring>> words;
+      int state = 0, pos=0;
+      for(auto key : text){
+          std::wcout << pos << ',' << key << ',' << state << std::endl;
+          state = transition(state, key);
+          std::wcout << state << std::endl;
+          collectOutputs(state, pos, words);
+          pos++;
+      }
+      return words;
+  }
 
  protected:
   // build trie Tree
   void buildTrieTree(std::vector<std::wstring> &keys, std::vector<V> &values){
+      this->m_keys = keys;
       this->m_values = values;
       for(auto i = 0; i < keys.size(); i++){
           this->m_root->addChild(keys[i], i);
       }
   }
 
-  void constructFailure();
-  void constructOutput();
+  void constructFailure(){
+      this->m_failure.resize(this->m_base.size()+1);
+      this->m_output.resize(this->m_base.size());
+      std::queue<ACDATNode*> nodes;
+
+      // Step 1: set this->root's child nodes
+      for(auto node : this->m_root->child_nodes){
+          this->m_failure[node->base_index] = this->m_root->base_index;
+          node->failure = this->m_root;
+          nodes.push(node);
+          constructOutput(node);
+      }
+
+      // Step 2: set other nodes (node's depth > 1)
+      ACDATNode *current_node, *target_node, *failure_node, *tmp_node;
+      while(!nodes.empty()){
+          current_node = nodes.front();
+          nodes.pop();
+          for(auto node : current_node->child_nodes){
+              target_node = node;
+              nodes.push(target_node);
+
+              // find target_node's failure node from current_node's failure node
+              failure_node = current_node->failure;
+              tmp_node = failure_node->getChild(target_node->chr);
+              while(!tmp_node){
+                  failure_node = failure_node->failure;
+                  if(!failure_node) break;
+                  tmp_node = failure_node->getChild(target_node->chr);
+              }
+              if(!failure_node){
+                  failure_node = this->m_root;
+              }else{
+                  failure_node = tmp_node;
+              }
+              target_node->failure = failure_node;
+              this->m_failure[target_node->base_index] = failure_node->base_index;
+              constructOutput(target_node);
+          }
+      }
+  }
+
+  void constructOutput(ACDATNode* node){
+      if(node->key_index == 0) return;
+      this->m_output[node->base_index] = node->key_index;
+  }
+
+  int transition(int state, wchar_t key){
+      auto iter = this->char2id.find(key);
+      if(iter == this->char2id.end()) return 0;
+
+      uint16_t key_id = iter->second;
+      int next_state = this->m_base[state] + key_id;
+
+      // 按照 success 表跳转
+      if(state + 1 == this->m_check[next_state]){
+          return next_state;
+      }
+
+      state = this->m_failure[state];
+      while(state != 0){
+          next_state =  this->m_base[state] + key_id;
+          if(state + 1 == this->m_check[next_state]){
+              return next_state;
+          }
+          state = this->m_failure[state];
+      }
+      return 0;
+  }
+
+  void collectOutputs(size_t state, size_t pos, std::vector<std::pair<size_t, std::wstring>> &words){
+      while(state != 0){
+          if(this->m_base[this->m_base[state]] < 0 && state+1 == this->m_check[this->m_base[state]])
+              words.emplace_back(pos, this->m_keys[this->m_output[this->m_base[state]]-1]);
+          state = this->m_base[this->m_failure[state]];
+      }
+
+  }
 
   // build double array trie from trie Tree
   void buildDoubleArrayTrie(){
@@ -187,6 +292,7 @@ class AhoCorasickDoubleArrayTrie{
               sub_begin = insertSibling(sub_siblings, begin + siblings[i].first + 1, used);
               this->m_base[begin + siblings[i].first] = sub_begin;
           }
+          siblings[i].second->base_index = begin + siblings[i].first;
       }
       siblings.clear();
       return begin;
@@ -200,12 +306,13 @@ class AhoCorasickDoubleArrayTrie{
  private:
   ACDATNode *m_root{};
 
-  std::vector<int> m_base{0};
-  std::vector<int> m_check{0};
+  std::vector<int> m_base;
+  std::vector<uint> m_check;
+  std::vector<std::wstring> m_keys;
   std::vector<V> m_values;
 
-  std::vector<std::vector<std::wstring>> output;
-  std::vector<int> failure;
+  std::vector<size_t> m_output;
+  std::vector<size_t> m_failure;
 
   std::unordered_map<wchar_t, uint16_t> char2id{{0,0}};
   std::unordered_map<uint16_t, wchar_t> id2char{{0,0}};
